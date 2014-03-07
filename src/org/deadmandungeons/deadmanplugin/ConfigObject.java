f9f0ffab08ca74eb2bc0d86b76bb27d00ffd3935
Weasel_Squeezer <deadmandungeons@gmail.com>
2014-03-07 18:28:24 -0500

    Added loadSigns method to ConfigObject interface

--- src/org/deadmandungeons/deadmanplugin/ConfigObject.java
+++ src/org/deadmandungeons/deadmanplugin/ConfigObject.java
@@ -41,9 +41,17 @@ public abstract class ConfigObject {
 	public abstract void updateConfig();
 	
 	/**
-	 * This method will be called by the super constructor, and is meant to load the properties from the map
+	 * This method should be called to load any DeadmanSigns that represent this object and to store
+	 * the signs location data in its respective SignEventListener sign Map
+	 * @param data - The ConfigurationSection containing the DeadmanSign data for this object type
+	 * @throws InvalidConfigurationException when the provided data is invalid for this type
+	 */
+	public abstract void loadSigns(ConfigurationSection data) throws InvalidConfigurationException;
+	
+	/**
+	 * This method will be called by the super constructor, and is meant to load the properties from the config
 	 * to the objects properties. Throw InvalidConfigurationException when the provided data is invalid
-	 * @param data - A map containing the ConfigurationSection keys and values for this ConfigObject
+	 * @param data - The ConfigurationSection containing the data for this object
 	 * @throws InvalidConfigurationException when the provided data is invalid for this type
 	 */
 	public abstract void load(ConfigurationSection data) throws InvalidConfigurationException;
